8cccedc456938adcae40907790c5100777173dde
jnyfah <jnyfaah@gmail.com>
2022-03-21 17:25:40 +0100

    about page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Next.js Blog Starter
+# Jnyfah.github.io
 A custom Next.js blog forked from  [`sandypockets`](https://github.com/sandypockets/nextjs-blog-starter). This blog is based on the original blog starter provided by Next, but includes a few extra features and performance improvements that are nice to have right out of the box. 
 
 ## Preview
--- pages/about.jsx
+++ pages/about.jsx
@@ -3,12 +3,35 @@ import Container from '../components/Layout/Container'
 import Layout from '../components/Layout/Layout'
 import { BLOG_NAME } from '../lib/constants'
 import PageHeading from '../components/PageHeading'
-import CartIcon from '../public/assets/blog/Capture3.JPG';
 
 export default function About() {
   return (
-    <div>
-      <img src="assets/blog/Capture3.JPG" alt="BigCo Inc. logo"/>
-    </div>
-  );
+    <Layout>
+      <Head>
+        <title>About | {BLOG_NAME}</title>
+      </Head>
+      <Container>
+        <PageHeading>kobayashi maru ...</PageHeading>
+        <section className="max-w-3xl mx-auto py-8 md:py-16 lg:py-24">
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 0 }}>
+        <div style={{ 
+      backgroundColor: 'rgba(52, 52, 52, 0.8)'
+    }}>
+          <br/>
+          <p>&nbsp; &#128188; &nbsp;&nbsp; SWE @ Microsoft</p>
+          <br/>
+          <p>&nbsp; &#127760; &nbsp;&nbsp; Jupiter</p>
+          <br/>
+          <p>&nbsp; &#129337; &nbsp;&nbsp; C/C++, Python and Rust</p>
+          <br/>
+          <p>&nbsp; &#11088; &nbsp;&nbsp; she/her</p>
+       </div>
+       <div>
+       <img src="assets/blog/authors/avatar.jpg" alt="BigCo Inc. logo"/>
+       </div>
+  </div>
+        </section>
+      </Container>
+    </Layout>
+  )	
 }
