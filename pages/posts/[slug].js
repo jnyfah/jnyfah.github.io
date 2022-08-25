68411875431c276643a318bce7023518102be3f9
Jennifer Chukwu <jnyfaah@gmail.com>
2022-08-25 23:05:55 +0000

    ogImage

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -29,7 +29,7 @@ export default function Post({ post, morePosts, preview }) {
                 <title>
                   {post.title} | {BLOG_NAME}
                 </title>
-                <meta property="og:image" content={post.ogImage.url} />
+                <meta property="og:image" content={post.ogImage} />
               </Head>
               <PostHeader
                 title={post.title}
