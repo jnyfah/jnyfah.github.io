9b0ffd7e3e2d19caf91c1bd4974352a64ff5f186
jnyfah <jnyfaah@gmail.com>
2023-01-25 10:32:18 +0000

    semantic analysis

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
         Kelechi.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-      Systems Engineering || Machine Learning || C++ || Rust
+      Systems Programming || Machine Learning || C++ || Rust
       </h4>
     </section>
   )
